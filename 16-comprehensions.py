3a881c23a574aff65bcd385e6628e378792bdfd1
larry-neil-1206 <larryneilmcgowen1206@gmail.com>
2024-03-05 05:13:23 -0700

    16-comprehensions.py

---
+++ 16-comprehensions.py
@@ -0,0 +1,26 @@
+# list comprehensions provide a short and concise way to create lists. 
+# It consists of square brackets containing an expression followed by a for clause, then zero or more for or if clauses. 
+# blueprint: variable = [out_exp for out_exp in input_list if out_exp == 2]
+
+multiples = [i for i in range(30) if i % 3 == 0]
+print(multiples)
+# Output: [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]
+
+# general way to create a list
+squared = []
+for x in range(10):
+    squared.append(x**2)
+    
+# list comprehension way to create a list
+squared = [x**2 for x in range(10)]
+
+# set comprehension
+squared = {x**2 for x in [1, 1, 2]}
+print(squared)
+# Output: {1, 4}
+
+multiples_gen = (i for i in range(30) if i % 3 == 0)
+print(multiples_gen)
+# Output: <generator object <genexpr> at 0x7fdaa8e407d8>
+for x in multiples_gen:
+  print(x)
